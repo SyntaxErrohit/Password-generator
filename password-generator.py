0b169d82a9ec560d41fb9126565c60685c228140
SyntaxErrohit <48488024+SyntaxErrohit@users.noreply.github.com>
2023-02-19 23:21:12 +0530

    made the condition function to work better

--- password-generator.py
+++ password-generator.py
@@ -13,13 +13,28 @@ def generate(length, numbers=True, spl_char=False):
         characters += punc
 
     password = ""
-    for i in range(length):
-        ch = random.choice(characters)
-        while ch in password:
-            ch = random.choice(characters)
-        password += ch
-    
+    met_condn = False
+    has_num = False
+    has_spl = False
+    while not met_condn or len(password) < length:
+        c = random.choice(characters)
+        while c in password:
+            c = random.choice(characters)
+        password += c
+
+        if c in digits:
+            has_num = True
+        elif c in punc:
+            has_spl = True
+
+        met_condn = True
+        if numbers:
+            met_condn = has_num
+        
+        if spl_char:
+            met_condn = met_condn and has_spl
+
     return password
 
-passwd = generate(10, True, True)
+passwd = generate(10, False, False)
 print(passwd)
